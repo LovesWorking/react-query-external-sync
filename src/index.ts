d6470c8c79a8c5c5fa8bfde413c4ee58d51d9021
lovesworking <austinlovesworking@gmail.com>
2025-04-08 07:18:34 -0600

    fix: update useSyncQueries import path to react-query-external-sync

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-export let useSyncQueries: typeof import("./new-sync/useSyncQueries").useSyncQueriesExternal;
+export let useSyncQueries: typeof import("./react-query-external-sync/useSyncQueries").useSyncQueriesExternal;
 // @ts-ignore process.env.NODE_ENV is defined by metro transform plugins
 if (process.env.NODE_ENV !== "production") {
-  useSyncQueries = require("./new-sync/useSyncQueries").useSyncQueries;
+  useSyncQueries =
+    require("./react-query-external-sync/useSyncQueries").useSyncQueries;
 } else {
   useSyncQueries = () => ({
     isConnected: false,
